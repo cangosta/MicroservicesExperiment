1b97b6de6d566eed3f16113beadd4225953bfccd
cangosta <luizcastro@gmail.com>
2015-08-24 21:43:24 +0100

    creating drivers microservice

---
+++ src/Experiments.DomainServices/Drivers/DriversDomainService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Experiments.DomainServices
+{
+    public class DriversDomainService : IDriversDomainService
+    {
+        private int nextDriver;
+        private List<Driver> drivers = null;
+
+        public DriversDomainService()
+        {
+            nextDriver = 0;
+            drivers = new List<Driver>();
+            drivers.Add(new Driver() {
+                Id = Guid.NewGuid(),
+                Name = "Luiz Alberto de Castro Santos"
+            });
+        }
+
+        public Driver CreateDriver(Driver driver)
+        {
+            driver.Id = Guid.NewGuid();
+            this.drivers.Add(driver);
+            return driver;
+        }
+
+        public IEnumerable<Driver> GetDrivers()
+        {
+            return this.drivers;
+        }
+
+        public Driver GetDrivers(Guid id)
+        {
+            return this.drivers.FirstOrDefault(d => d.Id == id);
+        }
+
+        public Driver GetNextDriver()
+        {
+            var driver = this.drivers.ElementAt(this.nextDriver);
+            this.nextDriver = (this.nextDriver + 1) % drivers.Count;
+            return driver;
+        }
+
+    }
+}
---
+++ src/Experiments.DomainServices/Drivers/IDriversDomainService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Experiments.DomainServices
+{
+    public interface IDriversDomainService
+    {
+        IEnumerable<Driver> GetDrivers();
+        Driver GetDrivers(Guid id);
+        Driver CreateDriver(Driver driver);
+        Driver GetNextDriver();
+    }
+}
